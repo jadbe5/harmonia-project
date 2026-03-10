30b3601329aeaefdb81bc2dde32189050e85c672
Jad Bendali <jad.bendali@epita.fr>
2026-03-10 21:03:08 +0100

    gui app main

--- harmonia/src/main.rs
+++ harmonia/src/main.rs
@@ -1,8 +1,23 @@
+mod gui;
+use gui::app::HarmoniaApp;
 
 fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([860.0, 480.0])
+            .with_min_inner_size([860.0, 480.0])
+            .with_max_inner_size([860.0, 480.0])
+            .with_title("TrueTone")
+            .with_resizable(false)
+            .with_fullscreen(false),
+        ..Default::default()
+    };
     eframe::run_native(
-        "Harmonia",
-        eframe::NativeOptions::default(),
-        Box::new(|_cc| Box::new(HarmoniaApp::default())),
+        "TrueTone",
+        options,
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+            Ok(Box::new(HarmoniaApp::default()) as Box<dyn eframe::App>)
+        }),
     )
 }
